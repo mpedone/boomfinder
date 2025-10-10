fe2b5a69725b89c7f6d79c880f5a456f8651a31a
Matthew Pedone <12798189+mpedone@users.noreply.github.com>
2025-10-10 16:09:06 -0400

    create a screen and test some pygame logic
    This code doesn't do much that is helpful, but I am learning pygame as
    I go, so this is all a way for me to start to test out some concepts.

--- main.py
+++ main.py
@@ -1,8 +1,40 @@
 import pygame
+import constants
 
 def main():
-    print("Hello from boomfinder!")
+    pygame.init()
+    clock = pygame.time.Clock()
 
+    screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+    x = 615
+    y = 335
+    running = True
+
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                x, y = pygame.mouse.get_pos()
+                x -= 25
+                y -= 25
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    running = False
+                    break
+                elif event.key == pygame.K_UP:
+                    y -= 50
+                elif event.key == pygame.K_DOWN:
+                    y += 50
+                elif event.key == pygame.K_LEFT:
+                    x -= 50
+                elif event.key == pygame.K_RIGHT:
+                    x += 50
+
+        pygame.Surface.fill(screen, (128, 128, 128))
+        pygame.draw.rect(screen,(255,0,128),(x,y,50,50))
+        pygame.display.flip()
+        clock.tick(60)
 
 if __name__ == "__main__":
     main()
