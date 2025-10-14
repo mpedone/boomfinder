694e3b9a0320fdec82610140273f5c670734afc3
Matthew Pedone <12798189+mpedone@users.noreply.github.com>
2025-10-14 16:47:23 -0400

    small changes to grid.py

--- grid.py
+++ grid.py
@@ -1,5 +1,7 @@
 '''
 In the simplest iteration, the game board will be a 10x10 grid of locations, represented by 100 tuples (0,0) through (9,9). I'll create an array of these locations, and then select ten at random to be bombs.
+
+How to get the bombs in place? Ten arrays of ten integers?
 '''
 import random
 
@@ -12,6 +14,20 @@ def bomb_placement(board):
         board.pop(board.index(loc))
     return bomb_locs
 
+def print_grid():
+    print("   1 2 3 4 5 6 7 8 9 10")
+    print(" 1 . . . . . . . . . .")
+    print(" 2 . . . . . . . . . .")
+    print(" 3 . . . . . . . . . .")
+    print(" 4 . . . . . . . . . .")
+    print(" 5 . . . . . . . . . .")
+    print(" 6 . . . . . . . . . .")
+    print(" 7 . . . . . . . . . .")
+    print(" 8 . . . . . . . . . .")
+    print(" 9 . . . . . . . . . .")
+    print("10 . . . . . . . . . .")
+
+
 
 def main():
     game_board_x = list(range(10))
@@ -19,6 +35,7 @@ def main():
 
     game_board = [(x, y) for x in game_board_x for y in game_board_y]
     bombs = bomb_placement(game_board)
+    print_grid()
     print(bombs)
 
 if __name__ == "__main__":
