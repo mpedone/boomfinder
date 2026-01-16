50877912186601ac419c1a5aa33fbcedf26af385
Matthew Pedone <12798189+mpedone@users.noreply.github.com>
2026-01-16 15:16:28 -0500

    remove unused code, add ascii art to readme

--- main.py
+++ main.py
@@ -4,39 +4,6 @@ import grid
 import sys
 
 def main():
-    """ pygame.init()
-    clock = pygame.time.Clock()
-
-    screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
-    x = 615
-    y = 335
-    running = True
-
-    while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                return
-            elif event.type == pygame.MOUSEBUTTONDOWN:
-                x, y = pygame.mouse.get_pos()
-                x -= 25
-                y -= 25
-            elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_ESCAPE:
-                    running = False
-                    break
-                elif event.key == pygame.K_UP:
-                    y -= 50
-                elif event.key == pygame.K_DOWN:
-                    y += 50
-                elif event.key == pygame.K_LEFT:
-                    x -= 50
-                elif event.key == pygame.K_RIGHT:
-                    x += 50
-
-        pygame.Surface.fill(screen, (128, 128, 128))
-        pygame.draw.rect(screen,(255,0,128),(x,y,50,50))
-        pygame.display.flip()
-        clock.tick(60) """
     board_width = 6
     board_height = 6
     
